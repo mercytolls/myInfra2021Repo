5b2c025ebdde714f33cff450525d2e463a52459a
mercytolls <129675940+mercytolls@users.noreply.github.com>
2023-05-09 15:11:46 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "mercy-first-tf-bucket"
     key = "main"
     region = "eu-west-2"
-    dynamodb_table = "my-dynamo-db-table"
+    dynamodb_table = "my-dynamodb-table"
   }
 }
